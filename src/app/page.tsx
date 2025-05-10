33d2a7e14eceb4ba04f37e67c89b6fe79e8ba7bc
Shogo Kitada <contact@shogo0x2e.com>
2025-05-11 00:07:14 +0900

    feat: discord entry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import DiscordEntry from "@/components/discord-entry";
+
 export default function Home() {
   return (
     <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
-      <iframe
+      <DiscordEntry />
+      {/* <iframe
         src="/unity/index.html"
         style={{ width: "100%", height: "100%", border: "none" }}
-      ></iframe>
+      ></iframe> */}
     </div>
   );
 }
